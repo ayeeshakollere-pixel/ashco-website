d41cc2d39cc58390dfe01a7fb61a232f00422c56
ayeeshakollere-pixel <ayeeshakollere@gmail.com>
2026-07-02 13:57:50 +0100

    Wrap routes in AuthProvider and add new routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
+import { AuthProvider } from './lib/auth';
 import Landing from './pages/Landing';
 import Order from './pages/Order';
+import Login from './pages/Login';
+import Account from './pages/Account';
+import Admin from './pages/Admin';
 
 // Scroll to top whenever the route changes
 function ScrollToTop() {
@@ -14,13 +18,18 @@ function ScrollToTop() {
 
 function App() {
   return (
-    <BrowserRouter>
-      <ScrollToTop />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/order" element={<Order />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/admin" element={<Admin />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
 
